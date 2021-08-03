d2cd20e346bfa40798ca2d12b4fb648211e925fc
xupeng_chen <xupeng_chen521@163.com>
2021-08-03 15:45:24 +0800

    正式的logging使用demo

--- test_logging.py
+++ test_logging.py
@@ -1,7 +1,20 @@
 import logging
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(filename)s[line:%(lineno)d] %(message)s", datefmt='%Y-%m-%d')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level=logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler = logging.FileHandler("log.txt")
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+
+    logger.addHandler(handler)
+    logger.addHandler(console)
+    
     logging.debug("This is debug message")
     logging.info("This is info message")
     logging.warning("This is warning message")

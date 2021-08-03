6e51f4f9ecc94c7018acec3cae01547c32f4899a
xupeng_chen <xupeng_chen521@163.com>
2021-08-03 15:35:16 +0800

    add logging py

---
+++ test_logging.py
@@ -0,0 +1,10 @@
+import logging
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(filename)s[line:%(lineno)d] %(message)s", datefmt='%Y-%m-%d')
+    logging.debug("This is debug message")
+    logging.info("This is info message")
+    logging.warning("This is warning message")
+    logging.error("This is error message")
+    logging.critical("This is critical message")
+    
